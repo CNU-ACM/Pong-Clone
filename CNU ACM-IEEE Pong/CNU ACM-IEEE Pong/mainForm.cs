8af0a7168a8ef1a1490e4161267d3077ccf5302d
Gerald McAlister <gerald.mcalister.gem@gmail.com>
2013-04-13 17:21:00 -0400

    Features and Comments Added
    -Add comments for better descriptions of objects.
    -Add the ball position and velocity variables.
    -Added the sprites for player 2 and the ball.
    -Started the timer event for gameplay.

--- CNU ACM-IEEE Pong/CNU ACM-IEEE Pong/mainForm.cs
+++ CNU ACM-IEEE Pong/CNU ACM-IEEE Pong/mainForm.cs
@@ -11,8 +11,29 @@ namespace CNU_ACM_IEEE_Pong
 {
     public partial class mainForm : Form
     {
+        /// <summary>
+        /// The velocity for the ball.
+        /// </summary>
+        Point ballVelocity = Point.Empty;
+        /// <summary>
+        /// The position for the ball.
+        /// </summary>
+        Point ballPosition = Point.Empty;
+
+        /// <summary>
+        /// The position of player 1.
+        /// </summary>
         Point player1Position = Point.Empty;
+        /// <summary>
+        /// The position of player 2.
+        /// </summary>
         Point player2Position = Point.Empty;
+
+        /// <summary>
+        /// The timer for the gameplay.  Updates
+        /// the gameplay every 1ms.
+        /// </summary>
+        Timer gamePlayTimer = new Timer();
 
         public mainForm()
         {
@@ -42,7 +63,7 @@ namespace CNU_ACM_IEEE_Pong
         }
 
         /// <summary>
-        /// Created by Elliot Rieflin.
+        /// Created by Gerald McAlister.
         /// This method loads the game's graphics and positions them to the correct spots.
         /// </summary>
         /// <param name="sender">Not used.</param>
@@ -50,6 +71,22 @@ namespace CNU_ACM_IEEE_Pong
         private void mainForm_Load(object sender, EventArgs e)
         {
             gameScreen.addSprite(0, Image.FromFile("images/player1.png"), 0, 0);
+            gameScreen.addSprite(1, Image.FromFile("images/player2.png"), gameScreen.Width - 32, 0);
+            gameScreen.addSprite(2, Image.FromFile("images/ball.png"), gameScreen.Width / 2 - 16, gameScreen.Height / 2 - 32);
+
+            gamePlayTimer.Interval = 1;
+            gamePlayTimer.Tick += new EventHandler(gamePlayTimer_Tick);
+            gamePlayTimer.Start();
+        }
+
+        /// <summary>
+        /// Created by Elliot Rieflin.
+        /// This method updates the gamemplay of the game.
+        /// </summary>
+        /// <param name="sender">Not used.</param>
+        /// <param name="e">Not used.</param>
+        void gamePlayTimer_Tick(object sender, EventArgs e)
+        {
         }
     }
 }
